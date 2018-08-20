96bae6155f8c8ebc2e93705c910b0e9d796ca53a
xunan <hiro26@163.com>
2018-08-20 22:02:39 +0100

    basic UI and styles;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EmployeeDashboardComponent
-  ],
+  declarations: [AppComponent, EmployeeDashboardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
