129237cd37bf2b83753c61a1637bea01aff1c494
xunan <hiro26@163.com>
2018-08-20 22:16:44 +0100

    implement fundamental services;

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  /**
+   * mock示例开源API
+   */
+  private baseUrl = environment.apiBaseUrl;
 
-  constructor() { }
+  constructor(private $http: HttpClient) {}
+
+  /**
+   * 测试只用到了GET方法，这里简单示例商业项目对接口服务的抽象思路
+   */
+  get<T>(path: string): Observable<T> {
+    return this.makeHttp('GET', path, null, null);
+  }
+
+  post<T>(path: string, body: Object): Observable<T> {
+    return this.makeHttp('POST', path, null, body);
+  }
+
+  private makeHttp<T>(
+    method: 'GET' | 'POST',
+    path: string,
+    headers: Object,
+    body: Object
+  ): Observable<T> {
+    const finalHeaders = Object.assign(
+      { 'Content-Type': 'application/json' },
+      headers
+    );
+
+    return this.$http.request<T>(method, this.baseUrl + path, {
+      body: body,
+      headers: finalHeaders
+    });
+  }
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+
+/**
+ * 常规项目里models一般放置在单独目录，这里仅作示例。
+ */
+export interface Employee {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+
+  expand?: boolean;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  constructor(private api: ApiService) {}
 
-  constructor() { }
+  getEmployees() {
+    return this.api.get<Employee[]>('users');
+  }
 }
